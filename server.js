ca4e605c3d6282413aaf68f13836348e442b294a
Purrro <45691888+Purrro@users.noreply.github.com>
2023-02-19 11:21:03 +0200

    .

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ app.use(cors());
 app.use(express.json());
 const dotenv = require("dotenv").config();
 
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const JWTStrategy = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const jwt = require('jsonwebtoken');
+
+
 const router = require("./routes/index");
 app.use("/", router);
 
